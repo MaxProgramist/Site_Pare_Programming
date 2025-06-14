bfef205c6ca501ea6e541b7e5b70a70c25924795
MaxProgramist <ls555431k@gmail.com>
2025-06-14 15:46:25 +0300

    Fixed all page (font) and spectator page

--- JSCode/JSCode_spectatorPage.js
+++ JSCode/JSCode_spectatorPage.js
@@ -3,6 +3,7 @@ const ROOM_CODE = sessionStorage.getItem("roomCode");
 const PLAYER_DIV_LIST = document.getElementById("playersGrid");
 
 let divToPlayer = [];
+let playersCircles = [];
 
 MakePlayersGroups();
 Loop();
@@ -20,68 +21,94 @@ function Delay(ms) {
 }
 
 async function SomeAsyncFunction() {
-    let payload = await LoadData();
+    let payload = await SendPost("RoomManager", "GetAllPlayers", { roomCode: ROOM_CODE });
 
-    if (payload.roomsCodes.length < 1) window.location.href = "index.html";
+    if (payload.status == 404 && payload.description == "No room with this code!") window.location.href = "index.html";
+    if (payload.status != 200) PopUpWindow(payload.description);
 
-    for (let i = 0; i < payload.rooms[ROOM_CODE].players.length; i++)
+    for (let i = 0; i < payload.players.length; i++)
         ChangePlayersScore(payload, i);
 }
 
 function ChangePlayersScore(payload, playerIndex) {
     let playerDiv = divToPlayer[playerIndex];
 
-    let playerScore = payload.rooms[ROOM_CODE].players[playerIndex].score;
-    let playerSkin = payload.rooms[ROOM_CODE].players[playerIndex].skin;
-    
-    let imgInsideDiv = playerDiv.querySelector("img");
-    let pInsideDiv = playerDiv.querySelectorAll("p");
-    imgInsideDiv.src = ICONS_LIST[playerSkin];
-    pInsideDiv[1].textContent = `${playerScore}/${payload.rooms[ROOM_CODE].maxCountOfTasks*100}`;
+    let playerScore = payload.players[playerIndex].score;
+
+    let pInsideDiv = playerDiv.querySelectorAll('div.spectator_progressCircle_text');
+    pInsideDiv[0].textContent = `${playerScore}/<br>${payload.rooms[ROOM_CODE].maxTasks * 100}`;
 }
 
 async function MakePlayersGroups() {
-    let payload = await LoadData();
+    let payload = await SendPost("RoomManager", "GetAllPlayers", { roomCode: ROOM_CODE });
 
-    if (payload.roomsCodes.length < 1) window.location.href = "index.html";
+    if (payload.status == 404 && payload.description == "No room with this code!") window.location.href = "index.html";
+    if (payload.status != 200) return PopUpWindow(payload.description);
 
     let usedPlayers = [];
-    for (let i = 0; i < payload.rooms[ROOM_CODE].players.length; i++) {
+    for (let i = 0; i < payload.players.length; i++) {
         if (usedPlayers.includes(i)) continue;
 
         NewPlayerIcon(payload, i);
-        NewPlayerIcon(payload, payload.rooms[ROOM_CODE].players[i].enemy);
+        NewPlayerIcon(payload, payload.players[i].enemy);
         usedPlayers.push(i);
         usedPlayers.push(payload.rooms[ROOM_CODE].players[i].enemy);
     }
 }
 
-function NewPlayerIcon(payload, playerIndex) {
-    let playerBox = document.createElement("div");
-    playerBox.setAttribute('class', 'admin_grid_item');
+function SetProgress(percentage, circleIndex) {
+    const CIRCLE = playersCircles[circleIndex];
+    const angle = (percentage / 100) * 360;
 
+    let color;
+    if (percentage < 50) {
+        const green = Math.round((percentage / 50) * 255);
+        color = `rgb(255, ${green}, 0)`;
+    } else {
+        const red = Math.round(255 - ((percentage - 50) / 50) * 255);
+        color = `rgb(${red}, 255, 0)`;
+    }
+
+    CIRCLE.style.setProperty('--percentage', `${angle}deg`);
+    CIRCLE.style.setProperty('--progress-color', color);
+}
+
+function NewPlayerIcon(payload, playerIndex) {
     let playerName = payload.players[playerIndex].name;
-    let playerScore = payload.players[playerIndex].score;
-    let playerSkin = payload.players[playerIndex].skin;
+    let playerSkin = payload.players[playerIndex].skin; 
+
+    let playerBox = document.createElement("div");
+    playerBox.setAttribute('class', 'classField_1 spectator_playerIcon');
 
     let playerBoxSkinImage = document.createElement("img");
     playerBoxSkinImage.src = ICONS_LIST[playerSkin];
     playerBoxSkinImage.setAttribute('class', 'universal_iconImage');
 
-    let playerBoxProfile = document.createElement("div");
+    let playerBoxName = document.createElement("span");
+    playerBoxName.innerText = `${playerName}`;
+    playerBoxName.setAttribute('class', 'spectator_playerIcon_name');
 
-    let playerBoxName = document.createElement("p");
-    playerBoxName.textContent = `${playerName}`;
-    let playerBoxScore = document.createElement("p");
-    playerBoxScore.textContent = `${playerScore}/${payload.rooms[ROOM_CODE].maxCountOfTasks*100}`;
+    let playerBoxScore = document.createElement("div");
+    playerBoxScore.innerText = `0/${payload.rooms[ROOM_CODE].maxCountOfTasks * 100}`;
+    playerBoxName.setAttribute('class', 'spectator_progressCircle_text');
 
-    playerBoxProfile.appendChild(playerBoxName);
-    playerBoxProfile.appendChild(playerBoxScore);
+    let progressCircle = document.createElement("div");
+    playerBox.setAttribute('class', 'spectator_progressCircle');
 
-    playerBox.appendChild(playerBoxSkinImage);
-    playerBox.appendChild(playerBoxProfile);
+    let playerInfo = document.createElement("div");
+    playerBox.setAttribute('class', 'spectator_playerIcon_info');
+
+    progressCircle.appendChild(playerBoxScore);
+
+    playerInfo.appendChild(playerBoxSkinImage);
+    playerInfo.appendChild(document.createElement("br"));
+    playerInfo.appendChild(playerBoxName);
+
+    playerBoxProfile.appendChild(playerInfo);
+    playerBoxProfile.appendChild(progressCircle);
 
     divToPlayer[playerIndex] = playerBox;
+    playersCircles[playerIndex] = progressCircle;
 
     PLAYER_DIV_LIST.appendChild(playerBox);
 }
